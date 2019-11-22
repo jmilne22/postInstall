cfa431585083b0e293fa69e6fdff005fe4177fb0
James Milne <jmilne22@gmail.com>
2019-11-22 16:23:26 +0200

    Added arch linux script

---
+++ postInstall-arch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo pacman -Syu
+
+sudo pacman -S git vim yay zsh --noconfirm
+
+# Start installing packages with yay AUR helper
+
+yay -S discord google-chrome slack oh-my-zsh-git visual-studio-code-bin --no-confirm
+
+# install spotify snap
+
+sudo snap install spotify
+
+
+# Set up git
+
+git config --global user.name "James Milne"
+git config --global user.email "jmilne22@gmail.com"
+
