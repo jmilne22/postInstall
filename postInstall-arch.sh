08e3372de5f78492003a48fb4d6a107378984624
James Milne <jmilne22@gmail.com>
2019-12-04 20:53:13 +0200

    Update postInstall-arch.sh
    Adding extra packages.

--- postInstall-arch.sh
+++ postInstall-arch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo pacman -Syu
+sudo pacman -Syu --no-confirm
 
-sudo pacman -S git vim yay zsh --noconfirm
+sudo pacman -S git vim yay zsh steam telegram-desktop --noconfirm
 
 # Start installing packages with yay AUR helper
 
@@ -12,9 +12,7 @@ yay -S discord google-chrome slack oh-my-zsh-git visual-studio-code-bin --no-con
 
 sudo snap install spotify
 
-
 # Set up git
 
 git config --global user.name "James Milne"
 git config --global user.email "jmilne22@gmail.com"
-
