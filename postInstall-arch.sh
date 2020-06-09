a885313a85da5b6a795ed80b6fda355605cd0683
James Milne <jmilne22@gmail.com>
2020-06-10 01:50:52 +0300

    Update postInstall-arch.sh
    Adding more things to arch.

--- postInstall-arch.sh
+++ postInstall-arch.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+# update system
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S git vim yay zsh steam telegram-desktop --noconfirm
+# install basics
+
+sudo pacman -S git vim yay zsh steam telegram-desktop flatpak bluez bluez-util --noconfirm
 
 # Start installing packages with yay AUR helper
 
-yay -S discord google-chrome slack oh-my-zsh-git visual-studio-code-bin --noconfirm
-
-# install spotify snap
-
-sudo snap install spotify
+yay -S discord google-chrome slack-desktop oh-my-zsh-git visual-studio-code-bin balena-etcher spotify --noconfirm
 
 # Set up git
 
 git config --global user.name "James Milne"
 git config --global user.email "jmilne22@gmail.com"
+
+# clone zsh repo
+
+git clone https://github.com/jmilne22/dotfiles.git
